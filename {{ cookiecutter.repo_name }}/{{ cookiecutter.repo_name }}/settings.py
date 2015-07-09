37e6b982b0800c6e6a738d15d388323b386583ba
Ryan McDevitt <mcdevitt.ryan@gmail.com>
2015-07-09 11:08:49 -0400

    removed secret key

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/settings.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^q76ojhw7uue1)d32myt#25x2dyljz#6-q91pxw@22y)wfze#7'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
